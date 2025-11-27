a7a089472784a89719836e2de3a55c25dc0c06a1
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2025-11-27 23:44:58 +0500

    седьмой коммит

--- src/main/java/com/example/LearningHub/service/UserService.java
+++ src/main/java/com/example/LearningHub/service/UserService.java
@@ -3,8 +3,12 @@ package com.example.LearningHub.service;
 
 import com.example.LearningHub.dto.request.UserRequestDto;
 import com.example.LearningHub.dto.response.StudentResponseDto;
+import com.example.LearningHub.entity.User;
+import com.example.LearningHub.exception.NotFoundException;
+import com.example.LearningHub.mapper.StudentMapper;
 import com.example.LearningHub.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -12,8 +16,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public StudentResponseDto postStudent(UserRequestDto dto){
+        userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(()-> new NotFoundException("Пользователь с email " + dto.getEmail() + "существует!"));
+
+        User user = StudentMapper.toEntity(dto);
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        userRepository.save(user);
+        return StudentMapper.toDto(user);
 
     }
 }
