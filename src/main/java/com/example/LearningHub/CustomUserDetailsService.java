de5039ee4f31097405cf925701fc257573e0094b
Асанали <168503373+Malkenov@users.noreply.github.com>
2025-12-05 06:47:38 +0500

    Add files via upload

---
+++ src/main/java/com/example/LearningHub/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.LearningHub.AppConfig;
+
+import com.example.LearningHub.entity.User;
+import com.example.LearningHub.repository.UserRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.CustomSQLErrorCodesTranslation;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    public final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found!"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
---
+++ src/main/java/com/example/LearningHub/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.LearningHub.AppConfig;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/create/student", "/users/create/teacher").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                )
+                .httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
